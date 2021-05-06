c489851aa253eb5f6278e0e14b5a6136a51d166b
Mickey <mickey@Mickeys-MacBook-Pro.local>
2021-05-06 13:43:38 -0500

    updated design

--- src/components/KCard.js
+++ src/components/KCard.js
@@ -7,14 +7,14 @@ import {
 const KCard = (props) => {
     const {image, title, content} = props
   return (
-    <div>
+    <div className="mb-3">
       <Card>
         <CardImg top width="100%" src={image} alt="Card image cap" />
         <CardBody>
           <CardTitle tag="h5">{title}</CardTitle>
           <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
           <CardText>{content}</CardText>
-          <Button>Button</Button>
+          {/* <Button>Button</Button> */}
         </CardBody>
       </Card>
     </div>
