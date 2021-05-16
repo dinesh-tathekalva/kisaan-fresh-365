9a800ebc2086dfdd0812dbe0e3ee98bc60a25922
Mickey <mickey@Mickeys-MacBook-Pro.local>
2021-05-16 02:03:27 -0500

    updated title

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,7 +16,7 @@ const NavBar = (props) => {
     return (
         <div>
             <Navbar color="light" light expand="md">
-                <NavbarBrand href="/"><p className="mainTitle">Kisaan Fresh 365</p></NavbarBrand>
+                <NavbarBrand href="/"><p className="mainTitle">RR Kisaan Fresh 365</p></NavbarBrand>
                 <NavbarToggler onClick={toggle} />
                 <Collapse isOpen={isOpen} navbar>
                     <NavbarText><span className="fas fa-camera"></span> <a className="phoneFont" href='https://wa.me/c/919493233615' role="button">+91 9493233615</a></NavbarText>
